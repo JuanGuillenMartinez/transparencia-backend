d1c7ebe83c4f71c4153928457119a873ac8acdad
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-05-22 10:30:38 -0500

    Commit inicial

---
+++ database/migrations/2022_05_22_015547_create_folder_groups_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('folder_groups', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('subdepartment_id')->constrained();
+            $table->string('serie');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('folder_groups');
+    }
+};
---
+++ database/migrations/2022_05_22_015654_create_folder_covers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('folder_covers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('folder_id')->constrained();
+            $table->string('area');
+            $table->string('asunto');
+            $table->date('fecha_inicio');
+            $table->date('fecha_terminacion');
+            $table->string('valor_documental');
+            $table->string('conservacion_tramite');
+            $table->string('conservacion_concentracion');
+            $table->string('conservacion_acceso')->nullable();
+            $table->string('conservacion_desclasificacion')->nullable();
+            $table->enum('clasificacion_informacion', ['confidencial', 'reservado'])->nullable();
+            $table->string('expediente')->nullable();
+            $table->string('localizacion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('folder_covers');
+    }
+};
