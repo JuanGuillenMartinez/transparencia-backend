e514bf3015616a547f3db597c9a4e92f7a0ec64c
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-05-24 20:14:06 -0500

    Registro de carpetas terminado

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\JsonResponse;
+use App\Models\Folder;
+use App\Models\FolderCover;
 use Illuminate\Http\Request;
 
 class FolderController extends Controller
@@ -24,7 +27,29 @@ class FolderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $folder = Folder::create([
+            'folder_group_id' => $request->folder_group_id,
+            'legajo' => $request->legajo,
+            'subserie' => $request->subserie,
+        ]);
+        if (isset($folder)) {
+            $folderCover = FolderCover::create([
+                'folder_id' => $folder->id,
+                'area' => $request->area,
+                'asunto' => $request->asunto,
+                'fecha_inicio' => $request->fecha_inicio,
+                'fecha_terminacion' => $request->fecha_terminacion,
+                'valor_documental' => $request->valor_documental,
+                'conservacion_tramite' => $request->conservacion_tramite,
+                'conservacion_concentracion' => $request->conservacion_concentracion,
+                'conservacion_acceso' => $request->conservacion_acceso,
+                'conservacion_desclasificacion' => $request->conservacion_desclasificacion,
+                'clasificacion_informacion' => $request->clasificacion_informacion,
+                'expediente' => $request->expediente,
+                'localizacion' => $request->localizacion,
+            ]);
+        }
+        return JsonResponse::sendResponse($folderCover);
     }
 
     /**
